32791b99fe6658d83ebc408a1d8bef621e3ea45b
Jackie Tao <jackie8tao@outlook.com>
2025-05-06 22:03:50 +0800

    feat:补充文件读取逻辑

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
 
 // definitions in lex.yy.c file
 extern FILE* yyin; 
@@ -6,7 +8,41 @@ extern char* yytext;
 extern int yylineno;
 extern int yylex(void);
 
+// print error message
+void errmsg(const char* format, ...) {
+  char errmsg[128];
+  va_list args;
+  va_start(args, format);
+  if (vsprintf(errmsg, format, args)<0) {
+    perror("cannot format error message");
+    exit(-99);
+  }
+  va_end(args);
+  perror(errmsg);
+  return;
+}
+
+// read input files, then print result
 int main(int argc, char **argv) {
-  printf("Hello World\n");
+  if (argc < 2) {
+    perror("empyt input files");
+    return -1;
+  }
+
+  FILE* fp;
+  for (int i=0; i<argc; i++) {
+    fp = fopen(argv[i], "r");
+    if (!fp) {
+      errmsg("cannot open file %s", argv[i]);
+      return -2;
+    }
+    yyin = fp;
+    yylineno = 0;
+
+    while(yylex() != EOF) {
+      printf("lineno: %d, token: %s\n", yylineno, yytext);
+    }
+  }
+
   return 0;
 }
