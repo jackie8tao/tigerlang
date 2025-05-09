2584a8ba500fb4697850cc76c188c5d37971724a
Jackie Tao <jackie8tao@outlook.com>
2025-05-09 20:46:55 +0800

    feat:添加parser文件

--- include/token.h
+++ include/token.h
@@ -4,26 +4,32 @@
 // token definitions
 typedef enum token
 {
-    TK_IDENT = 1, // identifier
-    TK_INTEGER,   // integer
-    TK_STRING,    // a sequence of characters
-    TK_ARRAY,     // array
-    TK_BREAK,     // break
-    TK_DO,        // do
-    TK_ELSE,      // else
-    TK_END,       // end
-    TK_FOR,       // for
-    TK_FUNCTION,  // function
-    TK_IF,        // if
-    TK_IN,        // in
-    TK_LET,       // let
-    TK_NIL,       // nil
-    TK_OF,        // of
-    TK_THEN,      // then
-    TK_TO,        // to
-    TK_TYPE,      // type
-    TK_VAR,       // var
-    TK_WHILE,     // while
+    YYEOF = 0,
+    // start at 258
+    TK_IDENT = 258, // identifier
+    TK_INTEGER,     // integer
+    TK_STRING,      // a sequence of characters
+
+    TK_ARRAY, // array
+    TK_NIL,   // nil
+
+    TK_VAR,      // var
+    TK_TYPE,     // type
+    TK_LET,      // let
+    TK_END,      // end
+    TK_FUNCTION, // function
+    TK_IN,       // in
+    TK_OF,       // of
+
+    TK_IF,    // if
+    TK_THEN,  // then
+    TK_ELSE,  // else
+    TK_BREAK, // break
+    TK_DO,    // do
+    TK_WHILE, // while
+    TK_FOR,   // for
+    TK_TO,    // to
+
     TK_COMMA,     // ,
     TK_COLON,     // :
     TK_SEMICOLON, // ;
@@ -34,19 +40,23 @@ typedef enum token
     TK_LBRACE,    // {
     TK_RBRACE,    // }
     TK_DOT,       // .
-    TK_PLUS,      // +
-    TK_MINUS,     // -
-    TK_MULTI,     // *
-    TK_DIV,       // /
-    TK_EQU,       // =
-    TK_NEQU,      // <>
-    TK_LT,        // <
-    TK_LEQU,      // <=
-    TK_GT,        // >
-    TK_GEQU,      // >=
-    TK_AND,       // &
-    TK_OR,        // |
-    TK_ASSIGN     // :=
+
+    TK_PLUS,  // +
+    TK_MINUS, // -
+    TK_MULTI, // *
+    TK_DIV,   // /
+
+    TK_EQU,  // =
+    TK_NEQU, // <>
+    TK_LT,   // <
+    TK_LEQU, // <=
+    TK_GT,   // >
+    TK_GEQU, // >=
+
+    TK_AND, // &
+    TK_OR,  // |
+
+    TK_ASSIGN // :=
 } token_t;
 
 #endif
